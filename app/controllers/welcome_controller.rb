9b3ac3376ee89e0f93a5bbcc0ec9a9d2d0a6a4b5
timeril <merrill9@gmail.com>
2014-01-08 16:11:11 -0500

    Initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+	def about_us
+  end
+  
+  def contact
+  end
+
+  def cities
+  end
+
+  def blog
+  end
+
+  def article
+  end
+  
+  def help
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Ticketzen::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  get 'about-us' => 'welcome#about_us'
+  get 'contact' => 'welcome#contact'
+  get 'cities' => 'welcome#cities'
+  get 'blog' => 'welcome#blog'
+  get 'article' => 'welcome#article'
+  get 'help' => 'welcome#help'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
